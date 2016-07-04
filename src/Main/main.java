303dc0aee48d6cde4f8ee251efb79c77a425e873
romanoks <werwolf959@gmail.com>
2016-07-04 08:33:36 +0300

    Start up file

---
+++ src/Main/main.java
@@ -0,0 +1,11 @@
+package Main;
+
+/**
+ * Created by User on 04.07.2016.
+ */
+public class main {
+
+    public static void main (String [] args){
+        System.out.println("Let's go!!!");
+    }
+}
